6be06d4444dccbeee64035dfbc66338016a31333
Tran Tien van <tienvan0158@gmail.com>
2023-04-26 14:58:12 +0700

    f

--- submission_20120623/Code_20120623.java
+++ submission_20120623/Code_20120623.java
@@ -33,62 +33,64 @@ public class Code_20120623 {
             
             FileSplit fileSplit = (FileSplit)context.getInputSplit();
             String filename = fileSplit.getPath().getName();
-            // if (word_key.contains("id") && !word_key.contains("")){
-            //     ArrayList<String> current = new ArrayList<String>();
-            //     counts.put(word_key, current);
-            // }
+            String id = "id" + filename.replace("doc", "").replace(".txt", "") ;
 
-            
-            // for (String w : word_values) {
-            //     if (counts.containsKey(w)) {
-            //         ArrayList<String> current = counts.get(w);
-            //         current.add(word_key);
-            //         counts.put(w, current);
-            //     } else {
-            //         ArrayList<String> current = new ArrayList<String>();
-            //         current.add(word_key);
-            //         counts.put(w, current);
-            //     }
+            if (word_key.contains(id)){
+                return;
+            }
+
+            String[] word_values = value.toString().split(" ");
+            for (int i = 0; i < word_values.length - 1; i++) {
+                String word_temp = word_values[i] + " " + word_values[i+1];
+
+                if (counts.containsKey(word_temp)) {
+                    ArrayList<String> current = counts.get(word_temp);
+                    current.add(id);
+                    counts.put(word_temp, current);
+                } else {
+                    ArrayList<String> current = new ArrayList<String>();
+                    current.add(id);
+                    counts.put(word_temp, current);
+                }
                 
-            // }
-            context.write(new Text(word_key), new Text(filename));
+            }
             
         }
 
 
 
-        // @Override
-        // protected void cleanup(Context context) throws IOException, InterruptedException {
-        //     // Output the graph
-        //     for (Map.Entry<String, ArrayList<String>> entry : counts.entrySet()) {
-        //         ArrayList<String> myList = entry.getValue();
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            // Output the graph
+            for (Map.Entry<String, ArrayList<String>> entry : counts.entrySet()) {
+                ArrayList<String> myList = entry.getValue();
 
-        //         // Count the occurrences of each element in the ArrayList
-        //         HashMap<String, Integer> countMap = new HashMap<String, Integer>();
-        //         for (String element : myList) {
-        //             if (countMap.containsKey(element)) {
-        //                 countMap.put(element, countMap.get(element) + 1);
-        //             } else {
-        //                 countMap.put(element, 1);
-        //             }
-        //         }
+                // Count the occurrences of each element in the ArrayList
+                HashMap<String, Integer> countMap = new HashMap<String, Integer>();
+                for (String element : myList) {
+                    if (countMap.containsKey(element)) {
+                        countMap.put(element, countMap.get(element) + 1);
+                    } else {
+                        countMap.put(element, 1);
+                    }
+                }
 
-        //         // Find the element with the highest count
-        //         String mostFrequentElement = null;
-        //         int highestCount = 0;
-        //         for (Map.Entry<String, Integer> entry2 : countMap.entrySet()) {
-        //             if (entry2.getValue() > highestCount) {
-        //                 mostFrequentElement = entry2.getKey();
-        //                 highestCount = entry2.getValue();
-        //             }
-        //         }
+                // Find the element with the highest count
+                String mostFrequentElement = null;
+                int highestCount = 0;
+                for (Map.Entry<String, Integer> entry2 : countMap.entrySet()) {
+                    if (entry2.getValue() > highestCount) {
+                        mostFrequentElement = entry2.getKey();
+                        highestCount = entry2.getValue();
+                    }
+                }
 
 
-        //         word.set(entry.getKey());
-        //         word_value.set(mostFrequentElement);
-        //         context.write(word, word_value);
-        //     }
-        // }
+                word.set(entry.getKey());
+                word_value.set(mostFrequentElement);
+                context.write(word, word_value);
+            }
+        }
     }
 
     public static class IntSumReducer
@@ -97,8 +99,6 @@ public class Code_20120623 {
         private Text result = new Text();      
         public void reduce(Text key, Iterable<Text> values,
                 Context context) throws IOException, InterruptedException {
-            
-
             
             for (Text calculated_value : values) {
                 result.set(calculated_value);
